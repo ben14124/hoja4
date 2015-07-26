20cf000dc90772ed47336b633ebf7383b657bc51
Daniela <Daniela@192.168.1.7>
2015-07-25 18:10:27 -0600

    Contadores no utilizados removidos

--- Hoja2/src/hoja2/NuestraCalculadora.java
+++ Hoja2/src/hoja2/NuestraCalculadora.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+package hoja2;
 
 /**
  *
@@ -18,20 +19,20 @@ public class NuestraCalculadora implements Calculadora{
   int contador, contador2; //contador para saber cuantas veces recorrer el for del stack
   int numero;
   int numero1, numero2;
+  
+  @Override
   public void setString(String linea){
       this.linea = linea;
   }
 
+  @Override
   public void meterVector(){
     contador = 0;
     for (int x=linea.length()-1; x>=0; x--){
       if((linea.charAt(x)==42) || (linea.charAt(x)==43) || (linea.charAt(x)==45) || ((linea.charAt(x))>=47) && (linea.charAt(x)<=57)){ //si son signos o numeros
          String s1 = Character.toString(linea.charAt(x));
-          //int a = Integer.parseInt(s1);
           stack1.push(s1);
           contador++;
-          //System.out.println(contador);
-          //stack1.peek();
       }
       else if(linea.charAt(x)==32){
 
@@ -43,109 +44,75 @@ public class NuestraCalculadora implements Calculadora{
       //=stack1
   }
 
+  @Override
   public void calcularVector(){ //try-catch NumberFormatException
-    for (int x = 0; x<contador; x++){ //cambie 1
-      //System.out.println(x);
+    for (int x = 0; x<contador; x++){
         String s2 = stack1.pop();
-        //System.out.println(s2);
+        
         try {
           //if ((s2.equals("0")) || (s2.equals("1")) || (s2.equals("2")) || (s2.equals("3")) || (s2.equals("4")) || (s2.equals("5")) || (s2.equals("6")) || (s2.equals("7")) || (s2.equals("8")) || (s2.equals("9"))){
             numero = Integer.parseInt(s2);
             stack2.push(numero);
-            //System.out.println("AAA" + numero);
             contador2++;
-          //}
         }
+        
         catch (NumberFormatException s){
-          //for (int z = 0; z<contador2-1; z++){
-          if (s2.equals("+")){
-            numero1 = stack2.pop();
-            numero2 = stack2.pop();
-            resultado = (numero2 + numero1);
-            //System.out.println(resultado);
-            String s3 = Integer.toString(resultado);
-            stack1.push(s3);
-            contador++; //cada vez que entramos en un signo hay que subirle 1 al contador porque luego sigue el ciclo y x sube de valor cuando "no lo deberia"
-            //contador2--;
-          }
-
-          if (s2.equals("-")){
-            numero1 = stack2.pop();
-            numero2 = stack2.pop();
-            resultado = (numero2 - numero1);
-            //System.out.println(resultado);
-            String s3 = Integer.toString(resultado);
-            //System.out.println("holi");
-            stack1.push(s3);
-            contador++;
-            //contador2--;
-          }
-
-          if (s2.equals("*")){
-            numero1 = stack2.pop();
-            numero2 = stack2.pop();
-            resultado = (numero2 * numero1);
-            //System.out.println(resultado);
-            String s3 = Integer.toString(resultado);
-            stack1.push(s3);
-            contador++;
-            //contador2--;
-          }
-
-          if (s2.equals("/")){
-            numero1 = stack2.pop();
-            numero2 = stack2.pop();
-            try {
-              resultado = (numero2 / numero1);
-              contador++;
-              //System.out.println(resultado);
-              String s3 = Integer.toString(resultado);
-              stack1.push(s3);
-              //contador2--;
+            if (s2.equals("+")){
+                numero1 = stack2.pop();
+                numero2 = stack2.pop();
+                resultado = (numero2 + numero1);
+                //System.out.println(resultado);
+                String s3 = Integer.toString(resultado);
+                stack1.push(s3);
+                contador++; //cada vez que entramos en un signo hay que subirle 1 al contador porque luego sigue el ciclo y x sube de valor cuando "no lo deberia"
             }
-            catch (ArithmeticException e){
-              //Error: division por cero
+
+            if (s2.equals("-")){
+                numero1 = stack2.pop();
+                numero2 = stack2.pop();
+                resultado = (numero2 - numero1);
+                String s3 = Integer.toString(resultado);
+                stack1.push(s3);
+                contador++;
             }
-            if ((stack2.empty())==false){
-              for (int y = 0; y==contador; y++){
-                String s4 = Integer.toString(stack2.pop());
-                stack1.push(s4);
-              }
+
+            if (s2.equals("*")){
+                numero1 = stack2.pop();
+                numero2 = stack2.pop();
+                resultado = (numero2 * numero1);
+                String s3 = Integer.toString(resultado);
+                stack1.push(s3);
+                contador++;
             }
-          }
-        //}//for
+
+            if (s2.equals("/")){
+                numero1 = stack2.pop();
+                numero2 = stack2.pop();
+                try {
+                  resultado = (numero2 / numero1);
+                  contador++;
+                  //System.out.println(resultado);
+                  String s3 = Integer.toString(resultado);
+                  stack1.push(s3);
+                }
+                catch (ArithmeticException e){
+                  //Error: division por cero
+                }
+                if ((stack2.empty())==false){
+                    for (int y = 0; y==contador; y++){
+                        String s4 = Integer.toString(stack2.pop());
+                        stack1.push(s4);
+                    }
+                }
+            }
         }
-        //System.out.println("letra: " + s2);
     }
-
    }
 
-
-
+  @Override
   public int getResultado(){
     return resultado;
 
   }
 
 }
-  
-/*Desde el main recorrer el Archivo y guardar cada linea en una variable (String) y mandarla a la clase calculadora. En la clase calculadora tener estos métodos:
-  setString(); que sirve para meter el String en una variable para luego recorrer la variable 
-  meterVector(); separamos el string en un vector
-  calcularVector(); aqui hacemos las operaciones y lo almacenamos en una variable para el getResultado
-  getResultado(int Resultado); saca el resultado para mandarlo al main
-*/
-/*
-  String linea = "2 3 4 2 3 2 3 4";
-        for (int x = 0; x<linea.length(); x++){
-                if(linea.charAt(x)!=32){
-        String s = Character.toString(linea.charAt(x));
-        int a = Integer.parseInt(s);
-                        System.out.println(a);
-        //stack1.push(Integer.parseInt(linea.charAt(x)));
-        //stack1.peek();
-      }
-                if(linea.charAt(x)==32){
-                    System.out.println("espacio");
-                }
-        }*/
