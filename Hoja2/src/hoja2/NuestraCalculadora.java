3e1a05173e8fcac67a58fc9b535b9f93a8a028c8
danielapa <dapocasangre@gmail.com>
2015-07-26 19:55:51 -0600

    Error ingreso signo de primero

--- Hoja2/src/hoja2/NuestraCalculadora.java
+++ Hoja2/src/hoja2/NuestraCalculadora.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-package hoja2;
 
 /**
  *
@@ -14,23 +13,22 @@ public class NuestraCalculadora implements Calculadora{
 
   Stack<String> stack1 = new StackVector<String>();
   Stack<Integer> stack2 = new StackVector<Integer>();
-  String linea;
+  String linea, primero;
   int resultado;
   int contador, contador2; //contador para saber cuantas veces recorrer el for del stack
   int numero;
   int numero1, numero2;
+  boolean calcular;
   
-  @Override
   public void setString(String linea){
       this.linea = linea;
   }
 
-  @Override
   public void meterVector(){
     contador = 0;
     for (int x=linea.length()-1; x>=0; x--){
       if((linea.charAt(x)==42) || (linea.charAt(x)==43) || (linea.charAt(x)==45) || ((linea.charAt(x))>=47) && (linea.charAt(x)<=57)){ //si son signos o numeros
-         String s1 = Character.toString(linea.charAt(x));
+          String s1 = Character.toString(linea.charAt(x));
           stack1.push(s1);
           contador++;
       }
@@ -44,72 +42,89 @@ public class NuestraCalculadora implements Calculadora{
       //=stack1
   }
 
-  @Override
-  public void calcularVector(){ //try-catch NumberFormatException
-    for (int x = 0; x<contador; x++){
-        String s2 = stack1.pop();
-        
-        try {
-          //if ((s2.equals("0")) || (s2.equals("1")) || (s2.equals("2")) || (s2.equals("3")) || (s2.equals("4")) || (s2.equals("5")) || (s2.equals("6")) || (s2.equals("7")) || (s2.equals("8")) || (s2.equals("9"))){
-            numero = Integer.parseInt(s2);
-            stack2.push(numero);
-            contador2++;
-        }
-        
-        catch (NumberFormatException s){
-            if (s2.equals("+")){
-                numero1 = stack2.pop();
-                numero2 = stack2.pop();
-                resultado = (numero2 + numero1);
-                //System.out.println(resultado);
-                String s3 = Integer.toString(resultado);
-                stack1.push(s3);
-                contador++; //cada vez que entramos en un signo hay que subirle 1 al contador porque luego sigue el ciclo y x sube de valor cuando "no lo deberia"
-            }
-
-            if (s2.equals("-")){
-                numero1 = stack2.pop();
-                numero2 = stack2.pop();
-                resultado = (numero2 - numero1);
-                String s3 = Integer.toString(resultado);
-                stack1.push(s3);
-                contador++;
-            }
-
-            if (s2.equals("*")){
-                numero1 = stack2.pop();
-                numero2 = stack2.pop();
-                resultado = (numero2 * numero1);
-                String s3 = Integer.toString(resultado);
-                stack1.push(s3);
-                contador++;
-            }
-
-            if (s2.equals("/")){
-                numero1 = stack2.pop();
-                numero2 = stack2.pop();
-                try {
-                  resultado = (numero2 / numero1);
-                  contador++;
-                  //System.out.println(resultado);
-                  String s3 = Integer.toString(resultado);
-                  stack1.push(s3);
-                }
-                catch (ArithmeticException e){
-                  //Error: division por cero
-                }
-                if ((stack2.empty())==false){
-                    for (int y = 0; y==contador; y++){
-                        String s4 = Integer.toString(stack2.pop());
-                        stack1.push(s4);
-                    }
-                }
-            }
-        }
-    }
+  public boolean calcularVector(){ //try-catch NumberFormatException
+  	  primero = stack1.pop();//saca el primero para verificar si es signo
+  	  
+  	  if(primero.equals("+") || primero.equals("-") || primero.equals("*") || primero.equals("/")){
+  	  	  calcular=false; //si el primero es signo, no se realiza operacion
+  	  }
+  	  
+  	  else{
+  	  	  calcular=true;
+  	  	  stack1.push(primero);
+  	  }
+  	  
+  	  if(calcular==true){
+	    for (int x = 0; x<contador; x++){
+		String s2 = stack1.pop();
+		
+		try {
+		  //if ((s2.equals("0")) || (s2.equals("1")) || (s2.equals("2")) || (s2.equals("3")) || (s2.equals("4")) || (s2.equals("5")) || (s2.equals("6")) || (s2.equals("7")) || (s2.equals("8")) || (s2.equals("9"))){
+		    numero = Integer.parseInt(s2);
+		    stack2.push(numero);
+		    contador2++;
+		}
+		
+		catch (NumberFormatException s){
+		    if (s2.equals("+")){
+			numero1 = stack2.pop();
+			numero2 = stack2.pop();
+			resultado = (numero2 + numero1);
+			//System.out.println(resultado);
+			String s3 = Integer.toString(resultado);
+			stack1.push(s3);
+			contador++; //cada vez que entramos en un signo hay que subirle 1 al contador porque luego sigue el ciclo y x sube de valor cuando "no lo deberia"
+		    }
+	
+		    if (s2.equals("-")){
+			numero1 = stack2.pop();
+			numero2 = stack2.pop();
+			resultado = (numero2 - numero1);
+			String s3 = Integer.toString(resultado);
+			stack1.push(s3);
+			contador++;
+		    }
+	
+		    if (s2.equals("*")){
+			numero1 = stack2.pop();
+			numero2 = stack2.pop();
+			resultado = (numero2 * numero1);
+			String s3 = Integer.toString(resultado);
+			stack1.push(s3);
+			contador++;
+		    }
+	
+		    if (s2.equals("/")){
+			numero1 = stack2.pop();
+			numero2 = stack2.pop();
+			try {
+			  resultado = (numero2 / numero1);
+			  contador++;
+			  //System.out.println(resultado);
+			  String s3 = Integer.toString(resultado);
+			  stack1.push(s3);
+			}
+			catch (ArithmeticException e){
+			  //Error: division por cero
+			}
+			if ((stack2.empty())==false){
+			    for (int y = 0; y==contador; y++){
+				String s4 = Integer.toString(stack2.pop());
+				stack1.push(s4);
+			    }
+			}
+		    }
+		}
+	    }
+	    return true;
+	  }
+	  
+	  else if(calcular==false){
+	  
+	  }
+	  return calcular;
    }
 
-  @Override
   public int getResultado(){
     return resultado;
 
