b45ab4b1bb631c8703fce632e661527a712af5b8
josaleriv <josaleriv@hotmail.com>
2015-07-28 10:45:01 -0600

    Update Calculadora.java

--- Hoja2/src/hoja2/Calculadora.java
+++ Hoja2/src/hoja2/Calculadora.java
@@ -6,16 +6,32 @@
 package hoja2;
 
 /**
- *
- * @author dbs_jd
+ *Esta clase es la interfaz del programa.
+ * @author Juan Diego Benitez, Daniela Pocasangre, Belen Hernandez y Alejandro Rivera
  */
 public interface Calculadora {
+    /**
+     * Indica la linea del documento de texto que se utilzara.
+     * @param linea Linea a utilizar en el codigo.
+     */
    public void setString(String linea);
 
+   /**
+    * Identifica si la linea contiene numeros o codigos, 
+    * tambien cuenta la cantidad de cada uno de ellos y los inserta en el vector.
+    */
    public void meterVector();
 
+   /**
+    * Sirve para comprobar que la linea ingresada no tenga ningun error.
+    * @return calcular: De tipo boolean. Contiene false si se tiene un error o true si es correcto.
+    */
    public boolean calcularVector();
 
+   /**
+    * Regresa el resultado de la linea de operaciones.
+    * @return resultado: De tipo integer. Contiene el resultado final de la linea.
+    */
    public int getResultado();
 
 }
